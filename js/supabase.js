8c50eb46f9b3780d16b9c55e3073071c7dc6aae8
seochoi0612-source <seochoi0612@gmail.com>
2026-08-16 18:53:26 +0900

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = "https://abprmzsxrgtcabxkmzgc.supabase.co";
-const SUPABASE_KEY = "여기에 지금 사용 중인 키 그대로";
+const SUPABASE_KEY = "sb_publishable_DnVN7TtI9sfIpIj-jZzbHQ_Zeygm1iU";
 
 window.supabaseClient = supabase.createClient(
     SUPABASE_URL,
